4421d1886d06502964238af03b2ba3d776919ff3
Nayan Prajapati <info.nayan1323@gmail.com>
2025-10-10 12:43:39 +0530

    group and single middleware

--- group-middleware/bootstrap/app.php
+++ group-middleware/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Agecheck;
+use App\Http\Middleware\CountryCheck;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +13,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // we can use as an group middleware
+        // $middleware->appendToGroup('group1',[
+        //     AgeCheck::class,
+        //     CountryCheck::class
+        // ]);
+        //we can use as an group and single middleware
+        $middleware->alias([
+            'agecheckd' => AgeCheck::class,
+            'countrycheck' => CountryCheck::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- group-middleware/routes/web.php
+++ group-middleware/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Alias;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::view('home', 'home')->middleware(['agecheckd','countrycheck']);
+Route::view('about', 'about');
+Route::view('contact','contact');
